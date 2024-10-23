fff57d7f4aa265cffbedbb1464c784c6187e8516
Shreyash <121010839+shreyashpawar8910@users.noreply.github.com>
2024-10-23 18:19:37 +0530

    Add files via upload

---
+++ coreJava/AllOperators.java
@@ -0,0 +1,51 @@
+package coreJava;
+
+
+public class AllOperators {
+
+	public static void main(String[] args) {
+		
+		// This is program for to demonstrate all the datatypes 
+		
+		int intVar = 10;   // declare integer data type
+		
+		short stortVar = 40;  // declare short datatype
+		
+		long longVar = 50;   // declare long variable
+		
+		byte byteVar = 4;   // declare byte data type 
+		
+		
+		float floatVar = 10.5F;   // declare floating data type
+		
+		double doubleVar = 14.144;  // declare double data type
+		
+		
+		boolean boolVar = true;  // declare boolean variable
+		
+		
+		char charVar = 'S';   // declare character data type
+		
+		String strVar = "Shreyash";  // declare String variable
+		
+		
+		
+		// Print all the variables values
+		
+		
+		System.out.println("Integer : "+intVar);
+		System.out.println("Short : "+stortVar);
+		System.out.println("Long : "+longVar);
+		System.out.println("Byte : "+byteVar);
+		System.out.println("Float : "+floatVar);
+		System.out.println("Double : "+doubleVar);
+		System.out.println("Boolean : "+boolVar);
+		System.out.println("Character : "+charVar);
+		System.out.println("String : "+strVar);
+		
+		
+
+	}
+
+}
+
